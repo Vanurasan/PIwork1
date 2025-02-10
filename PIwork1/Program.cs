4b68c0f2e94659108a39b9d3287b88ea3533a4d2
Vanurasan <Vanurasan@outlook.com>
2025-02-10 15:37:37 +0700

    Added first classes

--- PIwork1/Program.cs
+++ PIwork1/Program.cs
@@ -8,8 +8,48 @@ namespace PIwork1
 {
     internal class Program
     {
+        //Задание 1 Вариант 9
+        //Учебные занятия: дата, название аудитории (строка в характерном формате), имя преподавателя(строка)
+        public class Lesson {
+            public DateTime date { get; set; }
+            public string room { get; set; }
+            public string teacher { get; set; }
+
+            public virtual void Read() { }
+            public virtual void Write() { }
+
+        }
+
+        public class Practice : Lesson
+        {
+            public int numberOfTasks { get; set; }
+
+            public override void Read() { }
+            public override void Write() { }
+        }
+        public class Lab : Lesson 
+        {
+            public string equipment { get; set; }
+
+            public override void Read() { }
+            public override void Write() { }
+
+        }
+        public class Lecture : Lesson 
+        {
+            public int numberOfGroups { get; set; }
+
+            public override void Read() { }
+            public override void Write() { }
+        }
+
         static void Main(string[] args)
         {
+            Lesson[] lessons = new Lesson[] { };
+            while (true) 
+            {
+
+            }
 
         }
     }
